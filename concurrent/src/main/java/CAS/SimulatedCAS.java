543bf2226fe0fb377a08688fc8076be61c9f184e
aspde <1678566105@qq.com>
2022-12-19 10:37:34 +0800

    CAS

---
+++ concurrent/src/main/java/CAS/SimulatedCAS.java
@@ -0,0 +1,14 @@
+package CAS;
+/**CAS语义**/
+public class SimulatedCAS {
+
+    private int value;
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (oldValue == expectedValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+}
