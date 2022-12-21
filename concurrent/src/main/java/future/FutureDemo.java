cc836f1f53b9c2dea49511a350a44e7867128f16
aspde <1678566105@qq.com>
2022-12-22 02:33:43 +0800

    future

---
+++ concurrent/src/main/java/future/FutureDemo.java
@@ -0,0 +1,29 @@
+package future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+/**Future常用用法**/
+public class FutureDemo {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> future = service.submit(new CallableTask());
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(1000);
+            return new Random().nextInt();
+        }
+    }
+}
--- concurrent/src/main/java/thread_local/ThreadLocalDemo2.java
+++ concurrent/src/main/java/thread_local/ThreadLocalDemo2.java
@@ -5,45 +5,45 @@ public class ThreadLocalDemo2 {
     public static void main(String[] args) {
         new Service1().service1();
     }
-}
 
-class Service1 {
+    static class Service1 {
 
-    public void service1() {
-        User user = new User("方斌");
-        UserContextHolder.holder.set(user);
-        new Service2().service2();
+        public void service1() {
+            User user = new User("方斌");
+            UserContextHolder.holder.set(user);
+            new Service2().service2();
+        }
     }
-}
 
-class Service2 {
+    static class Service2 {
 
-    public void service2() {
-        User user = UserContextHolder.holder.get();
-        System.out.println("Service2拿到用户名：" + user.name);
-        new Service3().service3();
+        public void service2() {
+            User user = UserContextHolder.holder.get();
+            System.out.println("Service2拿到用户名：" + user.name);
+            new Service3().service3();
+        }
     }
-}
 
-class Service3 {
+    static class Service3 {
 
-    public void service3() {
-        User user = UserContextHolder.holder.get();
-        System.out.println("Service3拿到用户名：" + user.name);
-        UserContextHolder.holder.remove();
+        public void service3() {
+            User user = UserContextHolder.holder.get();
+            System.out.println("Service3拿到用户名：" + user.name);
+            UserContextHolder.holder.remove();
+        }
     }
-}
 
-class UserContextHolder {
+    static class UserContextHolder {
 
-    public static ThreadLocal<User> holder = new ThreadLocal<>();
-}
+        public static ThreadLocal<User> holder = new ThreadLocal<>();
+    }
 
-class User {
+    static class User {
 
-    String name;
+        String name;
 
-    public User(String name) {
-        this.name = name;
+        public User(String name) {
+            this.name = name;
+        }
     }
 }
